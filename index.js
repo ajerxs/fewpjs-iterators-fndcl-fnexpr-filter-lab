6b3fa6b4bc7a3714b0386e369c1dc2b5fcde4e36
Andrew Eames <ajerxs@gmail.com>
2021-12-12 15:40:27 -0500

    commit tree ffb85d8e13367e63077c3cdf52c971f81dafbbd2

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => {
+        if(driver.toLowerCase() === name.toLowerCase()) {
+            return driver;
+        }
+    });
+}
+
+function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(driver => {
+        if(driver.name === name) {
+            return driver.name;
+        }
+    })
+}
